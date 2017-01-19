2e9347f991808b562a6a33a866d54efa5b2470bb
Kirill <qurubo@gmail.com>
2017-01-19 20:35:35 +0300

    Getters, toString

--- src/main/java/ru/doublekdev/Main.java
+++ src/main/java/ru/doublekdev/Main.java
@@ -8,6 +8,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Currency currency = new Currency();
         //System.out.println(currency.toString());
+        System.out.println("--------------------------------------------------------------------------------------------");
         currency.info();
+        System.out.println("--------------------------------------------------------------------------------------------");
+        System.out.println("USD= "+currency.getSalUSDCurrency());
+        System.out.println("--------------------------------------------------------------------------------------------");
     }
 }
